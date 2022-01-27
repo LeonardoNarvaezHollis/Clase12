b637fa4644b54a749c6f473b59049dd78b08b53c
LeonardoNarvaezHollis <98190282+LeonardoNarvaezHollis@users.noreply.github.com>
2022-01-27 15:58:47 -0500

    Add files via upload

---
+++ proc12-AA1-plantilla-main/sketch.js
@@ -0,0 +1,20 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  for(var c=0; c<marks.length; c++){
+    if(marks[c]>=45){
+      console.log(marks[c])
+    }
+  }
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
